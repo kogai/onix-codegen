903eb15a42dd23449bd963f0079d983caa28a901
shinichi kogai <kogai0121@gmail.com>
2020-12-29 11:05:30 +0900

    init from existance implementation

---
+++ ir/onix.go
@@ -0,0 +1,27 @@
+package onix
+
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+)
+
+// Read read ONIX for Books 2.1 format file.
+func Read(input string) error {
+	file, err := ioutil.ReadFile(input)
+	if err != nil {
+		return err
+	}
+
+	var data IngramContentOnix
+	decoder := xml.NewDecoder(bytes.NewReader(file))
+	decoder.CharsetReader = func(label string, input io.Reader) (io.Reader, error) {
+		return input, nil
+	}
+
+	if err := decoder.Decode(&data); err != nil {
+		return err
+	}
+	return nil
+}
