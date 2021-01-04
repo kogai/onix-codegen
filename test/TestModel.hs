a69069c2a9e336ba4cf4aabc3938618a40425f2d
shinichi kogai <kogai0121@gmail.com>
2021-01-04 11:00:26 +0900

    add test of atom-ref

--- test/TestModel.hs
+++ test/TestModel.hs
@@ -4,7 +4,7 @@ module TestModel (tests) where
 
 import qualified Data.Map as M
 import Data.Text (Text, pack, unpack)
-import Model (Kind (Tag), dropDuplicate, model, models)
+import Model (Kind (Tag), dropDuplicate, model, models, typeToText)
 import Test.HUnit (Test (TestCase, TestList), assertEqual)
 import Text.XML (def, parseText, readFile)
 import Xsd
@@ -56,6 +56,39 @@ expected3 =
         [Documentation "Notification or update type code"]
     )
 
+expected4 =
+  TypeComplex
+    ( ComplexType
+        { complexAnnotations = [],
+          complexContent =
+            ContentSimple
+              ( SimpleContentExtension
+                  ( SimpleExtension
+                      { simpleExtensionBase = QName {qnNamespace = Just (Namespace {fromNamespace = "http://www.editeur.org/onix/2.1/reference"}), qnName = "List82"},
+                        simpleExtensionAttributes =
+                          [ InlineAttribute
+                              ( AttributeInline
+                                  { attributeInlineName = QName {qnNamespace = Just (Namespace {fromNamespace = "http://www.editeur.org/onix/2.1/reference"}), qnName = "refname"},
+                                    attributeInlineType = Ref (QName {qnNamespace = Just (Namespace {fromNamespace = "http://www.w3.org/2001/XMLSchema"}), qnName = "NMTOKEN"}),
+                                    attributeInlineFixed = Just "BibleContents",
+                                    attributeInlineUse = Optional
+                                  }
+                              ),
+                            InlineAttribute
+                              ( AttributeInline
+                                  { attributeInlineName = QName {qnNamespace = Just (Namespace {fromNamespace = "http://www.editeur.org/onix/2.1/reference"}), qnName = "shortname"},
+                                    attributeInlineType = Ref (QName {qnNamespace = Just (Namespace {fromNamespace = "http://www.w3.org/2001/XMLSchema"}), qnName = "NMTOKEN"}),
+                                    attributeInlineFixed = Just "b352",
+                                    attributeInlineUse = Optional
+                                  }
+                              )
+                          ]
+                      }
+                  )
+              )
+        }
+    )
+
 tests =
   [ TestCase
       ( do
@@ -65,7 +98,16 @@ tests =
       ),
     TestCase
       ( do
+          scm <- getSchema "./test/test_model_atomic.xsd"
+          let actual = typeToText expected1
+          assertEqual "can derive string type" "string" actual
+      ),
+    TestCase
+      ( do
           scm <- getSchema "./test/test_model_list.xsd"
+          print ""
+          (print . schemaTypes) scm
+          print ""
           let actual = (head . map snd . M.toList . schemaTypes) scm
           assertEqual "can parse simple list type" expected2 actual
       ),
@@ -73,6 +115,18 @@ tests =
       ( do
           scm <- getSchema "./test/test_model_atom_enum.xsd"
           let actual = (head . map snd . M.toList . schemaTypes) scm
-          assertEqual "can parse simple atomic type with union" expected3 actual
+          assertEqual "can parse simple atomic type of enum" expected3 actual
+      ),
+    TestCase
+      ( do
+          scm <- getSchema "./test/test_model_atom_ref.xsd"
+          let actual = (head . map snd . M.toList . schemaTypes) scm
+          assertEqual "can parse simple atomic type which reference to other type" expected4 actual
+      ),
+    TestCase
+      ( do
+          scm <- getSchema "./test/test_model_atom_ref.xsd"
+          let actual = typeToText expected4
+          assertEqual "can derive referenced type" "BibleContents" actual
       )
   ]
