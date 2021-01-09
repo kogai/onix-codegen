6118678c43f9b87a0603cd661aef3337edbbce09
shinichi kogai <kogai0121@gmail.com>
2021-01-09 12:42:42 +0900

    simplify test

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,31 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TestUtils (utilTests) where
+module TestUtils (makeTargetQName) where
 
 import Data.Text (Text, pack, unpack)
 import Model (Kind (Tag), dropDuplicate, model, models)
 import Test.HUnit (Test (TestCase, TestList), assertEqual)
+import Xsd
+  ( Namespace (Namespace, fromNamespace),
+    QName (QName, qnName, qnNamespace),
+  )
 
-models' =
-  [ model "m1" "M1" (Just "string") Tag False False [],
-    model "m2" "M2" (Just "string") Tag False False [],
-    model "m3" "M3" (Just "string") Tag False False [],
-    model "m4" "M4" (Just "string") Tag False False [],
-    model "m5" "M5" (Just "string") Tag False False [],
-    model "m6" "M6" (Just "string") Tag False False [],
-    model "m3" "M3" (Just "string") Tag False False [],
-    model "m3" "M3" (Just "string") Tag False False []
-  ]
-
-models'' =
-  [ model "m1" "M1" (Just "string") Tag False False [],
-    model "m2" "M2" (Just "string") Tag False False [],
-    model "m3" "M3" (Just "string") Tag True False [],
-    model "m4" "M4" (Just "string") Tag False False [],
-    model "m5" "M5" (Just "string") Tag False False [],
-    model "m6" "M6" (Just "string") Tag False False []
-  ]
-
-utilTests =
-  [ TestCase (assertEqual "dropDuplicate" models'' (dropDuplicate models'))
-  ]
+makeTargetQName :: Text -> QName
+makeTargetQName n =
+  QName
+    { qnNamespace = Just (Namespace {fromNamespace = "http://www.editeur.org/onix/2.1/reference"}),
+      qnName = n
+    }
